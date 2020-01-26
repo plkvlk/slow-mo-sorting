101036c185be14c681423059d6bba5b9ff5300b5
Pavol Kovalík <pavolkovalik@gmail.com>
2020-01-26 15:01:37 +0100

    removed unused vendors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './assets/vendors/fontawesome-free-5.12.0-web/css/all.min.css';
-import './assets/vendors/skeleton.css';
 import './assets/index.css';
 
 import {BarChart} from './bar-chart';
